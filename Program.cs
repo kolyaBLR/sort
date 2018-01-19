a28e28633a778bf7058aefce64d8ec9b5a9eb135
Nikolay Bobrov <bk97w@bk.ru>
2018-01-19 10:03:29 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
